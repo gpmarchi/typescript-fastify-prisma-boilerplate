332c14fc29553086c1b45ac186adb4114f0ffbe9
gpmarchi <gustavomarchi@gmail.com>
2023-07-04 13:43:46 -0300

    refactor: rename name property to title in entities

--- src/domain/entities/permission.ts
+++ src/domain/entities/permission.ts
@@ -2,19 +2,19 @@ import { randomUUID } from 'crypto'
 
 interface PermissionProps {
   slug: string
-  name: string
+  title: string
   description: string
 }
 
 export class Permission {
   public id: string
   public slug: string
-  public name: string
+  public title: string
   public description: string
 
   constructor(props: PermissionProps, id?: string) {
     this.slug = props.slug
-    this.name = props.name
+    this.title = props.title
     this.description = props.description
     this.id = id ?? randomUUID()
   }
--- src/domain/entities/role.ts
+++ src/domain/entities/role.ts
@@ -2,7 +2,7 @@ import { randomUUID } from 'crypto'
 
 interface RoleProps {
   slug: string
-  name: string
+  title: string
   description: string
   permissions?: string[]
 }
@@ -10,13 +10,13 @@ interface RoleProps {
 export class Role {
   public id: string
   public slug: string
-  public name: string
+  public title: string
   public description: string
   public permissions: string[]
 
   constructor(props: RoleProps, id?: string) {
     this.slug = props.slug
-    this.name = props.name
+    this.title = props.title
     this.description = props.description
     this.permissions = props.permissions ?? []
     this.id = id ?? randomUUID()
