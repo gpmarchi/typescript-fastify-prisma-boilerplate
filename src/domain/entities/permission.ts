11f0e3cf1f1d97c0c9d8a2e6e7fccd9bcc269887
gpmarchi <gustavomarchi@gmail.com>
2023-07-10 11:52:26 -0300

    refactor: add special method for creating instances

--- src/domain/entities/permission.ts
+++ src/domain/entities/permission.ts
@@ -1,10 +1,29 @@
 import { Entity } from '@/shared/entities/entity'
+import { UniqueEntityID } from '@/shared/entities/value-objects/unique-entity-id'
+import { Optional } from '@/shared/types/optional'
 import { Slug } from './value-objects/slug'
 
 interface PermissionProps {
   slug: Slug
   title: string
   description: string
+  createdAt: Date
+  updatedAt?: Date
 }
 
-export class Permission extends Entity<PermissionProps> {}
+export class Permission extends Entity<PermissionProps> {
+  static create(
+    props: Optional<PermissionProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const permission = new Permission(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id,
+    )
+
+    return permission
+  }
+}
--- src/domain/entities/role.ts
+++ src/domain/entities/role.ts
@@ -1,4 +1,6 @@
 import { Entity } from '@/shared/entities/entity'
+import { UniqueEntityID } from '@/shared/entities/value-objects/unique-entity-id'
+import { Optional } from '@/shared/types/optional'
 import { Slug } from './value-objects/slug'
 
 interface RoleProps {
@@ -6,6 +8,20 @@ interface RoleProps {
   title: string
   description: string
   permissions?: string[]
+  createdAt: Date
+  updatedAt?: Date
 }
 
-export class Role extends Entity<RoleProps> {}
+export class Role extends Entity<RoleProps> {
+  static create(props: Optional<RoleProps, 'createdAt'>, id?: UniqueEntityID) {
+    const role = new Role(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id,
+    )
+
+    return role
+  }
+}
