790ffc54a54f898503099ab63b7730be37bb5ea4
gpmarchi <gustavomarchi@gmail.com>
2023-07-12 13:30:33 -0300

    test: use describe to organize test suite

--- src/domain/users/enterprise/entities/value-objects/slug.spec.ts
+++ src/domain/users/enterprise/entities/value-objects/slug.spec.ts
@@ -1,14 +1,16 @@
-import { expect, test } from 'vitest'
+import { expect } from 'vitest'
 import { Slug } from './slug'
 
-test('it should be able to create a new slug from text', () => {
-  const slug = Slug.createFromText('Slug text')
+describe('Create Slug', () => {
+  it('should be able to create a new slug from text', () => {
+    const slug = Slug.createFromText('Slug text')
 
-  expect(slug.value).toEqual('slug-text')
-})
+    expect(slug.value).toEqual('slug-text')
+  })
 
-test('it should be able to create a new slug from invalid text', () => {
-  const slug = Slug.createFromText('Slug--text-')
+  it('should be able to create a new slug from invalid text', () => {
+    const slug = Slug.createFromText('Slug--text-')
 
-  expect(slug.value).toEqual('slug-text')
+    expect(slug.value).toEqual('slug-text')
+  })
 })
