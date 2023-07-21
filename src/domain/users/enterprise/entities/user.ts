0481439c3f7531103fbd4a7a92bf8c80fbd7a438
gpmarchi <gustavomarchi@gmail.com>
2023-07-21 14:05:53 -0300

    fix: set createdAt with provided date else set new date

--- src/domain/users/enterprise/entities/user.ts
+++ src/domain/users/enterprise/entities/user.ts
@@ -121,7 +121,7 @@ export class User extends Entity<UserProps> {
     const user = new User(
       {
         ...props,
-        createdAt: new Date(),
+        createdAt: props.createdAt ?? new Date(),
       },
       id,
     )
