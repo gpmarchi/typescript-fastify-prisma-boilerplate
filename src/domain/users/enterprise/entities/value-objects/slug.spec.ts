22cd43de053f4e93a11960967425fd68a13acd55
gpmarchi <gustavomarchi@gmail.com>
2023-08-02 17:32:54 -0300

    test: create slug from correctly formated slug text

--- src/domain/users/enterprise/entities/value-objects/slug.spec.ts
+++ src/domain/users/enterprise/entities/value-objects/slug.spec.ts
@@ -8,6 +8,12 @@ describe('Create Slug', () => {
     expect(slug.value).toEqual('slug-text')
   })
 
+  it('should be able to create a new slug from slug formatted text', () => {
+    const slug = Slug.createFromText('slug-text')
+
+    expect(slug.value).toEqual('slug-text')
+  })
+
   it('should be able to create a new slug from invalid text', () => {
     const slug = Slug.createFromText('Slug--text-')
 
