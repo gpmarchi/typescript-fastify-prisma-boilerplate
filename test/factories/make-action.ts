af2c4c24096d06b5172db003128c0c12ddc19740
gpmarchi <gustavomarchi@gmail.com>
2023-08-09 12:31:38 -0300

    feat: allow factories to override entity ids

--- test/factories/make-action.ts
+++ test/factories/make-action.ts
@@ -3,13 +3,19 @@ import { faker } from '@faker-js/faker'
 import { Action, ActionProps } from '@/domain/users/enterprise/entities/action'
 import { UniqueEntityID } from '@/shared/entities/value-objects/unique-entity-id'
 
-export function makeAction(override: Partial<ActionProps> = {}) {
-  const action = Action.create({
-    resourceId: new UniqueEntityID(),
-    title: faker.lorem.words(),
-    description: faker.lorem.sentence(),
-    ...override,
-  })
+export function makeAction(
+  override: Partial<ActionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const action = Action.create(
+    {
+      resourceId: new UniqueEntityID(),
+      title: faker.lorem.words(),
+      description: faker.lorem.sentence(),
+      ...override,
+    },
+    id,
+  )
 
   return action
 }
--- test/factories/make-user.ts
+++ test/factories/make-user.ts
@@ -1,19 +1,26 @@
 import { faker } from '@faker-js/faker'
 
 import { User, UserProps } from '@/domain/users/enterprise/entities/user'
+import { UniqueEntityID } from '@/shared/entities/value-objects/unique-entity-id'
 import { subYears } from 'date-fns'
 
-export function makeUser(override: Partial<UserProps> = {}) {
-  const user = User.create({
-    firstName: faker.person.firstName(),
-    lastName: faker.person.lastName(),
-    birthDate: subYears(new Date(), 40),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-    phone: faker.phone.number(),
-    roles: [],
-    ...override,
-  })
+export function makeUser(
+  override: Partial<UserProps> = {},
+  id?: UniqueEntityID,
+) {
+  const user = User.create(
+    {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      birthDate: subYears(new Date(), 40),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      phone: faker.phone.number(),
+      roles: [],
+      ...override,
+    },
+    id,
+  )
 
   return user
 }
