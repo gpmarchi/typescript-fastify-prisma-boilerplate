e83af7f2801bc2ac41eb64b7db2e8a1be36f91e7
gpmarchi <gustavomarchi@gmail.com>
2023-08-17 15:49:01 -0300

    feat: enable graceful shutdown

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -9,3 +9,25 @@ app
   .then(() => {
     console.log(`🚀 HTTP Server started on port ${env.PORT}!`)
   })
+
+enum Signals {
+  SIGHUP = 1,
+  SIGINT = 2,
+  SIGTERM = 15,
+}
+
+const shutdown = (signal: string, value: number) => {
+  console.log('Shutdown!')
+
+  app.close(() => {
+    console.log(`Server stopped by ${signal} with value ${value}`)
+    process.exit(128 + value)
+  })
+}
+
+Object.keys(Signals).forEach((signal) => {
+  process.on(signal, () => {
+    console.log(`Process received a ${signal} signal`)
+    shutdown(signal, Signals[signal as keyof typeof Signals])
+  })
+})
