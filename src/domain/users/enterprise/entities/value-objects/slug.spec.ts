85fd2dd5e0c549352c92ba2e9be52956c703135a
gpmarchi <gustavomarchi@gmail.com>
2024-08-08 11:07:21 -0300

    refactor: use new toString method instead of accessing value

--- src/domain/users/enterprise/entities/value-objects/slug.spec.ts
+++ src/domain/users/enterprise/entities/value-objects/slug.spec.ts
@@ -5,18 +5,18 @@ describe('Create Slug', () => {
   it('should be able to create a new slug from text', () => {
     const slug = Slug.createFromText('Slug text')
 
-    expect(slug.value).toEqual('slug-text')
+    expect(slug.toString()).toEqual('slug-text')
   })
 
   it('should be able to create a new slug from slug formatted text', () => {
     const slug = Slug.createFromText('slug-text')
 
-    expect(slug.value).toEqual('slug-text')
+    expect(slug.toString()).toEqual('slug-text')
   })
 
   it('should be able to create a new slug from invalid text', () => {
     const slug = Slug.createFromText('Slug--text-')
 
-    expect(slug.value).toEqual('slug-text')
+    expect(slug.toString()).toEqual('slug-text')
   })
 })
